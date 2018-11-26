bc5eb994f0091cad21b7b83bcb0e24f1ae0b63cc
Wingcfly <wesleynguyen1411@gmail.com>
2018-11-26 10:51:36 +0700

    Add Google Map API to DriverMapActivity

--- app/src/main/java/com/wingcfly/uber/DriverLoginActivity.java
+++ app/src/main/java/com/wingcfly/uber/DriverLoginActivity.java
@@ -36,7 +36,7 @@ public class DriverLoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null) {
-                    Intent aMap = new Intent(DriverLoginActivity.this, MapActivity.class);
+                    Intent aMap = new Intent(DriverLoginActivity.this, DriverMapActivity.class);
                     startActivity(aMap);
                     finish();
                     return;
